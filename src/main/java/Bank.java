3f0fa9a7ecd7fd91676cc7b1b35921197138e97d
Ralph Scronce <scronce@comcast.net>
2020-09-19 11:38:26 -0400

    Initial commit

---
+++ src/main/java/Bank.java
@@ -0,0 +1,31 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name = "banks_table")
+public class Bank {
+    @Id
+    //@Column(name = "id")
+    //@GeneratedValue(generator = "incrementor")
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //@Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
